ad40ed91a8979a92ad1ffdb8505c1da82e5c4910
Jeffrey Ting <jeffrey_ting@cargill.com>
2024-07-25 15:17:46 -0500

    context for vars

---
+++ context/UserContext.tsx
@@ -0,0 +1,66 @@
+import { createContext, useContext, useState } from "react";
+
+const STORAGE_KEY = "my-test";
+
+const getCurrentState = (): UserContextState | null => {
+  try {
+    const stringData = localStorage.getItem(STORAGE_KEY);
+    const parsedData = JSON.parse(stringData as string) as UserContextState;
+    return parsedData;
+  } catch {
+    return null;
+  }
+};
+
+const defaultState: UserContextState = {
+  sessionId: "",
+  state: "",
+};
+
+const UserContext = createContext<UserContextType | null>(null);
+
+export const UserContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [state, setState] = useState(getCurrentState() || defaultState);
+
+  const set = (newChanges: Partial<UserContextState>) => {
+    const newState = {
+      ...state,
+      ...newChanges,
+    };
+    setState(newState);
+  };
+
+  const save = () => {
+    // could also use cookies
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
+  };
+
+  return (
+    <UserContext.Provider value={{ state, set, save }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUserContext = (): UserContextType => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error("useUserContext must be used with a UserContextProvider");
+  }
+  return context;
+};
+
+export type UserContextType = {
+  state: UserContextState;
+  set: (newChanges: Partial<UserContextState>) => void;
+  save: () => void;
+};
+
+export type UserContextState = {
+  sessionId: string;
+  state: string;
+};
