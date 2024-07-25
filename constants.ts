93edc1ed5d97aae2fb29815c7fc33c63d8bbc362
Jeffrey Ting <jeffrey_ting@cargill.com>
2024-07-25 15:17:38 -0500

    constants to be used throughout

---
+++ constants.ts
@@ -0,0 +1,5 @@
+export const REDIRECT_URI = "http://localhost:3000/callback";
+export const TOKEN = "latestCSRFToken";
+export const STATE_LENGTH = 16;
+export const CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string;
+export const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string;
