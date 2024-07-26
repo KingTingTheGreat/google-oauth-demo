2efe65a2014d6239e3a5da0c379c2f1eea1681bf
Jeffrey Ting <jeffrey_ting@cargill.com>
2024-07-26 15:32:05 -0500

    suspese

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import Footer from '@/components/footer';
 import Header from '@/components/header';
 import { UserContextProvider } from '@/context/UserContext';
+import { Suspense } from 'react';
 
 export default function RootLayout({
   children,
@@ -9,30 +10,32 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <UserContextProvider>
-      <html lang="en">
-        <body
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            minHeight: '100vh',
-            justifyContent: 'space-between',
-          }}
-        >
-          <Header />
-          <div
+    <Suspense>
+      <UserContextProvider>
+        <html lang="en">
+          <body
             style={{
               display: 'flex',
               flexDirection: 'column',
-              justifyContent: 'start',
-              alignItems: 'center',
+              minHeight: '100vh',
+              justifyContent: 'space-between',
             }}
           >
-            {children}
-          </div>
-          <Footer />
-        </body>
-      </html>
-    </UserContextProvider>
+            <Header />
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'start',
+                alignItems: 'center',
+              }}
+            >
+              {children}
+            </div>
+            <Footer />
+          </body>
+        </html>
+      </UserContextProvider>
+    </Suspense>
   );
 }
